61378e24ce6e71175a074f8c19a9515aedbdb40e
Devansh1741 <rathoredevansh1741@gmail.com>
2024-12-18 17:00:18 +0530

    Setup

---
+++ app/account/page.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "Guest Area",
+};
+
+export default function Page() {
+  return (
+    <h2 className="mb-4 text-2xl font-semibold text-accent-400">
+      Welcome, Devansh
+    </h2>
+  );
+}
